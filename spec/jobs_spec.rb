438383475c8ec941e384beff5b49074f48d099bc
Michael Preston <michael.preston@onthebeach.co.uk>
2015-02-20 16:08:41 +0000

    Part two, three unordered jobs

--- spec/jobs_spec.rb
+++ spec/jobs_spec.rb
@@ -15,6 +15,20 @@ describe Job do
   end
 
   describe 'three jobs' do
+    let(:a) { Job.new('a') }
+    let(:b) { Job.new('b') }
+    let(:c) { Job.new('c') }
 
+    let(:job_list) { JobList.new }
+
+    before do
+      job_list.add(a)
+      job_list.add(b)
+      job_list.add(c)
+    end
+
+    it 'returns the three jobs unordered' do
+      expect(job_list.sorted_list).to eql('abc')
+    end
   end
 end
